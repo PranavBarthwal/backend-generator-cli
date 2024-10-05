ad8644c3482f3093b5347e5cef17a4550bba4c9d
partg952 <dankparth@gmail.com>
2024-10-06 02:59:18 +0530

    added connection and schema files as mongoose snippets

---
+++ snippets/mongoose-schema.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema({
+  key:String,
+});
+
+const model = mongoose.model("Model",schema);
+export default model;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+// src/constants.js
+
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    GITHUB_REPO: "https://github.com/PranavBarthwal/backend-generator-cli",
+    NPM_PACKAGE: "https://www.npmjs.com/package/backend-generator-cli"
+};
